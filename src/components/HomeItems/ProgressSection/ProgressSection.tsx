12bb96a0ee0dd7792dcb436a99d3a47dc98c53df
ChrisCoastal <contactchriscoastal@gmail.com>
2023-02-16 12:47:50 -0800

    add ProgressSection styles

--- src/components/HomeItems/ProgressSection/ProgressSection.tsx
+++ src/components/HomeItems/ProgressSection/ProgressSection.tsx
@@ -1,35 +1,56 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import DashView from 'src/assets/dash-view.jpg';
 import GameView from 'src/assets/game-view.jpg';
 
 import useIntersectionObserver from 'src/hooks/useIntersectionObserver';
+import downArrow1 from 'src/assets/arrow-down-1.svg';
+import downArrow2 from 'src/assets/arrow-down-2.svg';
 
 const ProgressSection = () => {
+  const [dashDetails, setDashDetails] = useState(false);
   const lightRef = useRef<HTMLDivElement>(null);
 
-  const isIntersect = useIntersectionObserver(lightRef, { threshold: 0.8 });
+  const isIntersect = useIntersectionObserver(lightRef, { threshold: 0.3 });
   const animate = isIntersect
     ? 'opacity-1 translate-y-0 translate-x-0'
     : 'opacity-0 translate-y-36 -translate-x-24';
 
+  const detailsVisible = dashDetails ? 'opacity-100' : 'opacity-0';
+
   return (
     <section className="flex flex-col items-center justify-center ">
-      <div className="mx-8 grid grid-cols-1 [perspective:900px] sm:mx-16 sm:w-[64rem] sm:grid-cols-5 sm:gap-8">
-        <div className="relative col-span-2 my-auto flex aspect-square flex-col items-center justify-center self-start rounded-full p-16 sm:col-start-1">
-          <h3 className="relative z-30 text-4xl font-bold text-stone-900 sm:mb-8">
+      <div className=" w-[80%] grid-rows-3 [perspective:900px] md:mx-16 md:grid md:grid-cols-5 md:grid-rows-3 md:gap-8 xl:w-[64rem]">
+        <div
+          onMouseEnter={() => setDashDetails(true)}
+          onMouseLeave={() => setDashDetails(false)}
+          className="relative transition-all duration-500 [transform:rotate3d(0,_-1,_0.1,_30deg)_translate(-8%)] hover:[transform:rotate3d(0,_-1,_0.1,_20deg)_translate(-5%)] md:col-span-3 md:col-start-3 md:row-span-2 md:row-start-1">
+          <img src={DashView} alt="player dashboard page" />
+          <div
+            className={`${detailsVisible} absolute -top-12 -left-48 flex rotate-3 gap-1 font-annie text-2xl tracking-wider text-stone-50 transition-all duration-500`}>
+            earn unique avatars and badges!
+            <img src={downArrow1} width="36px" className=" translate-y-2" />
+          </div>
+          <div
+            className={`${detailsVisible} absolute left-48 -top-14 flex rotate-6 gap-1 font-annie text-xl tracking-wider text-stone-50 transition-all duration-500`}>
+            track progress and rankings
+            <img src={downArrow2} width="36px" className=" translate-y-3" />
+          </div>
+          <div
+            className={`${detailsVisible} absolute left-24 -bottom-12 flex rotate-3 gap-1 font-annie text-xl tracking-wider text-stone-50 transition-all duration-500`}>
+            <img src={downArrow1} width="36px" className="-translate-y-3 rotate-180" />
+            join tournaments and leagues
+          </div>
+        </div>
+        <div className="relative my-auto flex aspect-square flex-col items-center justify-center self-start rounded-full p-16 md:col-span-2 md:col-start-1 md:row-span-2 md:row-start-1">
+          <h3 className="relative z-30 mb-2 text-4xl font-bold text-stone-900 xl:mb-8">
             Light up the Shadows!
           </h3>
-          <p className="text-md relative z-30 font-medium text-stone-900 sm:text-xl">
+          <p className="text-md relative z-30 font-medium text-stone-900 xl:text-xl">
             Get insight into your play with game analysis, stat tracking, strategy guides, and more.
           </p>
           <div
             ref={lightRef}
-            className={`${animate} absolute top-0 left-0 z-10 h-full w-full max-w-[98vw] rounded-full bg-stone-100 transition-all duration-[1200ms] sm:w-full `}>
-            <div className="absolute aspect-square w-full max-w-[98vw] rounded-full bg-clip-content shadow-[inset_0_0_0.6rem_0.8rem_rgb(28_25_23)] [height:102%] [top:-1%] [left:-1%]"></div>
-          </div>
-        </div>
-        <div className="col-span-3 col-start-1 shadow-[inset_0_0_0.4rem_0.8rem_rgb(28_25_23)] transition-all duration-500 [transform:rotate3d(0,_-2,_0.1,_30deg)] hover:[transform:rotate3d(0,_-2,_0.1,_20deg)] sm:col-start-3">
-          <img src={DashView} alt="player dashboard page" />
+            className={`${animate} absolute top-0 left-0 z-10 aspect-square h-full w-full max-w-[98vw] rounded-full bg-[radial-gradient(circle,_rgba(245,245,244,1)_64%,_rgba(28,25,25,0)_70%)] transition-all duration-[1200ms] sm:w-full `}></div>
         </div>
       </div>
 
--- src/components/HomeItems/SkillSection/SkillSection.tsx
+++ src/components/HomeItems/SkillSection/SkillSection.tsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState, useRef } from 'react';
 import Teeth from 'src/components/UI/icons/Teeth/Teeth';
 import useScrollY from 'src/hooks/useScrollY';
+import { AvatarSize } from 'src/@types';
+import Avatar from 'src/components/Avatar/Avatar';
 
 const SkillSection = () => {
   const scrollY = useScrollY();
@@ -23,8 +25,8 @@ const SkillSection = () => {
     <div
       ref={containerRef}
       className="relative my-48 bg-emerald-300 bg-gradient-to-br from-emerald-300 to-emerald-400 py-36">
-      <Teeth className="absolute" style={{ top: -80 }} />
-      <Teeth className="absolute -bottom-36 rotate-180" />
+      {/* <Teeth className="absolute" style={{ top: -80 }} /> */}
+      {/* <Teeth className="absolute -bottom-36 rotate-180" /> */}
       <div className="flex flex-col items-center">
         <h3 className="text-5xl font-bold text-stone-900 sm:text-6xl lg:text-7xl">
           Sharp teeth not required.
@@ -33,6 +35,29 @@ const SkillSection = () => {
           Find an opponent to fit your skill level and improve your play with post game analysis and
           strategy.
         </p>
+      </div>
+      <div className="mx-auto flex items-center justify-center gap-4">
+        <div className="inline-grid w-56 grid-cols-5 grid-rows-1">
+          <Avatar
+            className={`${AvatarSize.LG} col-span-2 col-start-1 row-start-1 border border-purple-500/60`}
+            avatar="🦁"
+          />
+          <Avatar
+            className={`${AvatarSize.LG} col-span-2 col-start-2 row-start-1 border border-purple-500/60`}
+            avatar="🦑"
+          />
+          <Avatar
+            className={`${AvatarSize.LG} col-span-2 col-start-3 row-start-1 border border-purple-500/60`}
+            avatar="🧙‍♂️"
+          />
+          <Avatar
+            className={`${AvatarSize.LG} col-span-2 col-start-4 row-start-1 border border-purple-500/60`}
+            avatar="👩‍🎤"
+          />
+        </div>
+        <h3 className="w-56 text-2xl font-semibold text-emerald-600 sm:w-64 sm:text-4xl">
+          Play thousands of users online
+        </h3>
       </div>
     </div>
   );
